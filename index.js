a78dcf56658dc7068b857298f774cd4afa5f43eb
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-10 23:27:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function showRepositories(event, data) {
-  console.log(this.responseText)
-  let repoList = "<ul>"
-    for(let i=0; i < this.responseText.length; i++) {
-      repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-    }
-  repoList += "</ul>"
+  // BEFORE
+  // console.log(this.responseText)
+  // let repoList = "<ul>"
+  //   for(let i=0; i < this.responseText.length; i++) {
+  //     repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  //   }
+  // repoList += "</ul>"
+
+  //AFTER: Parsing JSON
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
 
   document.getElementById("repositories").innerHTML = repoList
 }
